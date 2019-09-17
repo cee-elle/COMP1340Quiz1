37412ff263d944c5d3865e4296a4d42eb97033f7
Cindy Le <cindyle@cindys-mbp-3.wifi.bcit.ca>
2019-09-17 14:20:06 -0700

    first commit

---
+++ Question1/q1.js
@@ -0,0 +1,4 @@
+//What is the difference between github and git?
+
+//Github is online and free. It allows us to share our code online and have collaborate with others.
+//Git allows to make "checkpoints" and manage our code. We can also make changes and revert back to the original code.
